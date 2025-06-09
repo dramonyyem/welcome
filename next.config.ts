8651fa93cf3190bbfe2a334b8b9fc134ec2d691d
daramonyyem <yem.daramony@gmail.com>
2025-06-09 21:34:21 +0700

    s commit

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,15 @@
 import type { NextConfig } from "next";
 
+const repo = "welcome"; // change this to your GitHub repo name
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
